1b79f02a9c15b16f85481e65e734f0bba69543aa
HicaroD <hdanrlley1@gmail.com>
2024-06-25 14:56:03 -0300

    feat: adding class for easily access colors from Tractian application

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TractianColors {
+  static const white = Color(0xFFFFFFFF);
+
+  static const darkBlue = Color(0x17192DFF);
+  static const lightBlue = Color(0x2188FFFF);
+
+  static const lightGrey = Color(0xD8DFE6FF);
+  static const darkGrey = Color(0x77818CFF);
+
+  static const red = Color(0xED3833FF);
+  static const green = Color(0x52C41AFF);
+}
