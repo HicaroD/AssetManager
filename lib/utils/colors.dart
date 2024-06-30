848d707c228fa702386dd028f06653ef99fb6e2f
HicaroD <hdanrlley1@gmail.com>
2024-06-30 18:14:24 -0300

    fix: colors were incorrect

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class TractianColors {
   static const white = Color(0xFFFFFFFF);
 
-  static const darkBlue = Color(0x17192DFF);
-  static const lightBlue = Color(0x2188FFFF);
+  static const darkBlue = Color(0xFF17192D);
+  static const lightBlue = Color(0xFF2188FF);
 
-  static const lightGrey = Color(0xD8DFE6FF);
-  static const darkGrey = Color(0x77818CFF);
+  static const lightGrey = Color(0xFFD8DFE6);
+  static const darkGrey = Color(0xFF77818C);
 
-  static const red = Color(0xED3833FF);
-  static const green = Color(0x52C41AFF);
+  static const red = Color(0xFFED3833);
+  static const green = Color(0xFF52C41A);
 }
